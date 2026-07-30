e00617c7fd666f0c456b91112238c447384d8070
Samant-saini <samantsaini2003@gmail.com>
2026-07-30 15:00:31 +0530

    add Vercel domain to CORS allowed origins

--- project/src/main/java/com/example/project/config/SecurityConfig.java
+++ project/src/main/java/com/example/project/config/SecurityConfig.java
@@ -47,7 +47,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));   // React dev server
+        config.setAllowedOrigins(List.of(
+    "http://localhost:5173",
+    "https://777-sage-gamma.vercel.app"
+));   // React dev server
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
